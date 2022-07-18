78b2060963f32c008c7299625630a93e4a53bc67
Rameez <muhammadrameezahmed73@gmail.com>
2022-07-19 00:51:23 +0500

    Update Calculator

--- src/component.js/Calculator.js
+++ src/component.js/Calculator.js
@@ -1,58 +1,107 @@
-import React,{ useState }  from 'react'
+import React, { useState } from 'react'
 
 const Calculator = () => {
-  const [answer, setAnswer] = useState(0);
-
-  const num1=2;
-  const num2=4;
-
-
-   const clickHandler0=()=>{
-    setAnswer(num1+num2);
+  const [answer, setAnswer] = useState([]);
+  const [num1, setNum1] = useState();
+  const [num2, setNum2] = useState();
 
 
-   }
-   
-   const clickHandler1=()=>{
-    setAnswer(num1-num2);
+
+  const Submit = (event) => {
+    event.preventDefault();
+    
+
+    const setAnswer = {
+      
+        num1: num1,
+        num2: num2
+    };
+    setAnswer([answer]);
+
+}
+
+const change = (event) => {
+  setNum1(event.target.value);
+
+}
+const change1 = (event) => {
+  setNum2(event.target.value);
+
+}
 
 
-   }
-
-   
-   const clickHandler2=()=>{
-    setAnswer(num1*num2);
+  const clickHandler0 = () => {
+    setAnswer(Number(num1) + Number(num2));
 
 
-   }
+  }
 
-   
-   const clickHandler3=()=>{
-    setAnswer(num1/num2);
+  const clickHandler1 = () => {
+    setAnswer(num1 - num2);
 
 
-   }
-    const clear=()=>{
-      setAnswer(0);
-    }
+  }
+
+
+  const clickHandler2 = () => {
+    setAnswer(num1 * num2);
+
+
+  }
+
+
+  const clickHandler3 = () => {
+    setAnswer(num1 / num2);
+
+
+  }
+  const clear = () => {
+    setAnswer("");
+    setNum1("");
+    setNum2("");
+
+  }
+
+ 
 
 
   return (
     <>
-    <div>
-      <h1>Simple Calculator</h1>  
-    </div>
-    <p>{answer}</p>
- 
-    <button>{`First number is ${num1}`},{`Second number is ${num2}`}</button>
-    <br></br>
-    <br></br>
-    <button onClick={clickHandler0}>Add</button>
-    <button onClick={clickHandler1}>sub</button>
-    <button onClick={clickHandler2}>Mul</button>
-    <button onClick={clickHandler3}>Div</button>
-    <button onClick={clear}>Reset</button>
-  
+      <div>
+        <h1>Simple Calculator</h1>
+      </div>
+
+
+
+      <form onClick={Submit}>
+
+
+
+
+
+        <label>
+          Num1 <input type="number" name="name" value={num1} onChange={change} />
+        </label>
+        <br></br>
+
+        <label>
+          Num2 <input type="number" name="name" value={num2} onChange={change1} />
+        </label>
+        <br></br>
+
+        <div>
+          <button onClick={clickHandler0}>Add</button>
+          <button onClick={clickHandler1}>sub</button>
+          <button onClick={clickHandler2}>Mul</button>
+          <button onClick={clickHandler3}>Div</button>
+          <button onClick={clear}>Reset</button>
+          <br></br>
+          <button >{`Answer  ${answer}`}</button>
+
+        </div>
+
+      </form>
+
 
     </>
   )
