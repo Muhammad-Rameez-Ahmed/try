4f696ece0b9bb6f9c7a052208482e4f9561a087d
Rameez <muhammadrameezahmed73@gmail.com>
2022-07-19 19:32:12 +0500

    Frontend done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Calculator from './component.js/Calculator';
+import CalculatorApp from './component.js/CalculatorApp';
 
 function App() {
   return (
-    <Calculator/>
+    // <Calculator/>
+    <CalculatorApp/>
     
   );
 }
